0b73c5702dc531ac3af46e45b78d9a5f7378fc42
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2014-10-06 22:28:16 +0200

    Capture images at defined times

--- raspberry_pi/camera.py
+++ raspberry_pi/camera.py
@@ -3,11 +3,7 @@ from time import sleep
 import datetime
 
 
-def capture(camera):
-    now = datetime.datetime.now()
-    for i in range(10):
-        filename = "image_%d_%d_%d_%d.jpg" % (i, now.year, now.month, now.day)
-        camera.capture(filename)
+CAPTURE_TABLE = "8:00,9:00,10:00,11:00,12:00,13:00,14:00,15:00,16:00,17:00,18:00,19:00"
 
 
 def set_defaults(camera):
@@ -30,11 +26,30 @@ def set_defaults(camera):
     camera.crop = (0.0, 0.0, 1.0, 1.0)
 
 
+def capture_loop(camera):
+    while True:
+        for t in CAPTURE_TABLE.split(","):
+            hour, minute = map(int, t.split(":"))
+            sleep_until(hour, minute, 1)
+            now = datetime.datetime.now()
+            filename = "image_%d_%d_%d_%d_%d.jpg" % (now.year, now.month, now.day, hour, minute)
+            camera.capture(filename)
+
+
+def sleep_until(hour, minute, verbose=0):
+    now = datetime.datetime.now()
+    future = datetime.datetime(now.day, now.month, now.day, hour, minute)
+    delta = (future - now).seconds
+    if verbose:
+        print("Sleeping for %d seconds" % delta)
+    sleep(delta)
+
+
 if __name__ == "__main__":
     camera = picamera.PiCamera()
 
     set_defaults(camera)
-    capture(camera)
+    capture_loop(camera)
 
     #camera.start_recording("video.h264")
     #sleep(5)
