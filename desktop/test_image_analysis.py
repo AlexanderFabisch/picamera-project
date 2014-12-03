c6eeb6c3205b7e1adeef3d000f7bf79e909e85f7
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2014-12-04 00:15:36 +0100

    Detect lines in image

--- desktop/test_image_analysis.py
+++ desktop/test_image_analysis.py
@@ -6,6 +6,11 @@ from pytransform.rotations import *
 from pytransform.transformations import *
 from pytransform.camera import *
 from optimizer.python.cmaes import fmin
+from skimage.filter import sobel
+from skimage.color import rgb2gray
+from skimage.io import imshow
+from skimage.filter import threshold_otsu
+from skimage.transform import hough_line, hough_line_peaks
 
 
 # Camera pose in world frame
@@ -14,7 +19,7 @@ from optimizer.python.cmaes import fmin
 #cam2world = transform_from(matrix_from_euler_xyz([0.382 * np.pi, 0, 0]),
 #                           [0, -1.95, 1.47])
 e_xyz = np.array([0.12, -0.04, -0.02]) * np.pi
-p = np.array([-0.83, -0.37, 2.06])
+p = np.array([-0.83, -1.22, 2.06])
 cam2world = transform_from(matrix_from_euler_xyz(e_xyz), p)
 # Source: http://elinux.org/Rpi_Camera_Module#Technical_Parameters
 focal_length = 0.0036
@@ -47,7 +52,7 @@ if __name__ == "__main__":
     im = np.array(Image.open(filename))
 
     projection_args = {"sensor_size": sensor_size, "image_size": image_size,
-                       "focal_length": focal_length, "kappa": 0.0}
+                       "focal_length": focal_length}
 
     P_corners = np.array([[ 0.000, 0.0, 0, 1],
                           [-0.100, 0.6, 0, 1],
@@ -56,32 +61,61 @@ if __name__ == "__main__":
     P_image_corners = np.array([[464, 319],
                                 [421, 198],
                                 [245, 189],
-                                [143, 183]])
-    image_corners = world2image(P_corners, cam2world, **projection_args)
+                                [144, 183]])
 
     def objective(params):
         e_xyz = params[:3]
-        p = params[3:]
+        p = params[3:6]
+        kappa = params[-1]
         cam2world = transform_from(matrix_from_euler_xyz(e_xyz), p)
-        P_image = world2image(P_corners, cam2world, **projection_args)
+        P_image = world2image(P_corners, cam2world, kappa=kappa,
+                              **projection_args)
         error = np.linalg.norm(P_image - P_image_corners) ** 2
         return error
 
-    r = fmin(objective, "ipop", x0=np.hstack((e_xyz, p)), maxfun=10000,
-             log_to_stdout=True)
-    print r
+    bounds = np.array([[-2 * np.pi, 2 * np.pi],
+                       [-2 * np.pi, 2 * np.pi],
+                       [-2 * np.pi, 2 * np.pi],
+                       [-0.77, -0.9],
+                       [-1.15, -1.3],
+                       [2.0, 2.2],
+                       [0, 0.1]])
+    covariance = np.array([np.pi / 2, np.pi / 2, np.pi / 2, 0.1, 0.1, 0.1, 0.01])
+    r = fmin(objective, "ipop", x0=np.hstack((e_xyz, p, [0])), maxfun=2000,
+             log_to_stdout=True, covariance=covariance, bounds=bounds,
+             random_state=0)
+    print "Parameters: ", np.round(r[0], 3), ", SSE: ", np.round(r[1], 2)
     params = r[0]
-    cam2world = transform_from(matrix_from_euler_xyz(params[:3]), params[3:])
-    image_corners = world2image(P_corners, cam2world, **projection_args)
+    cam2world = transform_from(matrix_from_euler_xyz(params[:3]), params[3:6])
+    image_corners = world2image(P_corners, cam2world, kappa=params[-1],
+                                **projection_args)
 
     world_grid = make_world_grid(n_points_per_line=101)
-    image_grid = world2image(world_grid, cam2world, **projection_args)
+    image_grid = world2image(world_grid, cam2world, kappa=params[-1],
+                             **projection_args)
 
+    plt.figure()
+    img = rgb2gray(im)
+    sobel_image = sobel(img)
+    thresh = threshold_otsu(sobel_image)
+    imt = sobel_image > thresh
+    imshow(imt)
+
+    hspace, angles, dists = hough_line(imt)
+    hspace, angles, dists = hough_line_peaks(
+        hspace, angles, dists, threshold=150.0)
+    rows, cols = imt.shape
+    for _, angle, dist in zip(hspace, angles, dists):
+        y0 = (dist - 0 * np.cos(angle)) / np.sin(angle)
+        y1 = (dist - cols * np.cos(angle)) / np.sin(angle)
+        plt.gca().plot((0, cols), (y0, y1), "r")
+    plt.gca().axis((0, cols, rows, 0))
+    plt.gca().imshow(img, cmap=plt.cm.gray)
+
+    plt.figure()
+    draw_to_image(im, P_image_corners, color=[0, 0, 255])
     draw_to_image(im, image_grid)
     draw_to_image(im, image_corners)
+    plt.imshow(im)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.imshow(im)
-    ax.plot()
     plt.show()
